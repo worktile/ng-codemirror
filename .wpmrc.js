318959aba0be562a631ff8fa590b4ef7428e3719
why520crazy <why520crazy@163.com>
2022-05-17 22:44:29 +0800

    build: bump 12.0.0 and skip branch for release

--- .wpmrc.js
+++ .wpmrc.js
@@ -1,6 +1,9 @@
 module.exports = {
   allowBranch: ["master", "12.*", "13.*"],
   bumpFiles: ["package.json", "package-lock.json", "src/package.json"],
+  skip: {
+    branch: true
+  },
   hooks: {
     prerelease: "npm run build",
     prepublish: "npm run build",
